1d2f28803046b98ce5370afeeac494c0b89b1c66
Regan <reganshop243@gmail.com>
2026-09-01 22:10:29 -0700

    Add mobile PWA installation support

--- static/pwa/sw.js
+++ static/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dora-dental-static-v4';
+const CACHE = 'dora-dental-static-v5';
 const STATIC_ASSETS = ['/offline/', '/static/pwa/manifest.webmanifest', '/static/css/app.css', '/static/pwa/icon-192.png', '/static/pwa/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC_ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
